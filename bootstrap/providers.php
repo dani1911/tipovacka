5f43b9777d8636d5950c73ff694db425b2a7bbc1
dani1911 <dani.strba@gmail.com>
2026-06-22 22:09:00 +0200

    added telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\Backend1701dPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
